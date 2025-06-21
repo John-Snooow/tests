bd1376c556060c37f48992a1303ddbeb35ecb109
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-06-20 22:09:40 -0300

    beforeAll
    O método beforeAll é utilizado para executar uma função antes dos testes serem realizados. É possível definir variáveis e atribuir valores a elas antes dos testes, garantindo que as informações necessárias estejam disponíveis. Ao utilizar o beforeAll, é importante garantir que as variáveis estejam corretamente definidas para que os testes sejam executados com sucesso. A função beforeAll é útil para preparar o ambiente de teste e garantir que tudo esteja pronto para a execução dos testes.

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,11 +1,16 @@
 import { sum } from "./server"
 
 describe("sum 1", () => {
-    
+    let sumResult: number
+
+    beforeAll(() => {
+        sumResult = 10
+    })
+
     it("should do sum of 3 + 7 must be 10", () => {
         const result = sum (3, 7)
     
-        expect(result).toBe(10)
+        expect(result).toBe(sumResult)
     })
     
     
