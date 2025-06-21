317f7888aae28cbcf57eefba805179040103bc5f
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-06-20 22:15:43 -0300

    afterAll
    O afterAll é uma função que é executada após todos os testes, assim como o beforeAll é executado antes. Pode ser útil para redefinir variáveis ou limpar a memória após os testes. Por exemplo, se você precisar carregar um arquivo para simular um banco de dados, pode usar o afterAll para remover esse arquivo da memória. É uma maneira de garantir que tudo seja limpo e organizado após a execução dos testes.

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -5,12 +5,19 @@ describe("sum 1", () => {
 
     beforeAll(() => {
         sumResult = 10
+        console.log("Executado antes dos testes", sumResult)
+
+    })
+
+    afterAll(() => {
+        sumResult = 0
+        console.log("Executado depois dos testes", sumResult)
     })
 
     it("should do sum of 3 + 7 must be 10", () => {
         const result = sum (3, 7)
     
-        expect(result).toBe(sumResult)
+        expect(result).toBe(10)
     })
     
     
