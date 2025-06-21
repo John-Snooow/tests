8a3df0b5aac25ad8084e8c4b79e71f2fd8c8361f
John Frank <139596702+John-Snooow@users.noreply.github.com>
2025-06-20 21:08:32 -0300

    Agrupando testes
    Automatizados, aprenda a agrupar testes utilizando a função describe. Organize múltiplos cenários de uma mesma funcionalidade em "suites de testes", melhorando a legibilidade e a estrutura dos resultados dos seus testes. Essencial para manter seus arquivos de teste limpos e eficientes.

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,7 +1,36 @@
 import { sum } from "./server"
 
-test("sum", () => {
-    const result = sum (3, 7)
+describe("sum 1", () => {
+    
+    test("sum", () => {
+        const result = sum (3, 7)
+    
+        expect(result).toBe(10)
+    })
+    
+    
+    test("sum of  2 + 2 must be 4", () => {
+        const result = sum (2, 2)
+    
+        expect(result).toBe(4)   
+    })
 
-    expect(result).toBe(10)
+})
+
+
+describe("sum 2", () => {
+    
+    test("sum", () => {
+        const result = sum (30, 70)
+    
+        expect(result).toBe(100)
+    })
+    
+    
+    test("sum of  2 + 2 must be 4", () => {
+        const result = sum (20, 25)
+    
+        expect(result).toBe(45)   
+    })
+    
 })
